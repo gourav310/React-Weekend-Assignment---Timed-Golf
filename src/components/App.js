f12610f2aa182b6eb008268ba62afcf75e01e0c8
Gourav Khurana <gouravkhurana310@gmail.com>
2020-11-16 00:36:17 +0530

    cmnit

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,16 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { renderBall:false, time: 0, x: 0, y: 0,ballPosition: { left: "0px" ,top:"0px"} };
+    this.state = { renderBall:false, time: 0, x: 0, y: 0,timeStart:0};
     this.Arrow=this.Arrow.bind(this);
     this.buttonClickHandler = this.buttonClickHandler.bind(this)
+     this.timer= this.timer.bind(this);
+     this.funct=null;
   };
   Arrow(e){
+    if(this.state.x===250 && this.state.y===250){
+      clearInterval(this.funct);
+    }
     if(this.state.x!==250 || this.state.y!==250){
           if(e.keyCode===39){        
           if(this.state.renderBall){
@@ -38,28 +43,43 @@ class Timer extends React.Component {
   }
   componentDidMount() {
     document.addEventListener("keydown",this.Arrow);
+    
   }
-
+  startTimer(){
+      let count =0;
+      this.funct=setInterval(()=>{
+        count=count+1;
+        this.setState({timeStart:count});
+      },1*1000)
+    
+      
+    }
+  
+  timer(){
+    
+    return `${this.state.timeStart}`;
+  }
   componentWillUnmount() {
     
 
   }
   buttonClickHandler() {
     this.setState({renderBall:true});
-    const time= new Date();
-    console.log(time.value);
+//    const time= new Date();
+    this.startTimer();
 }
-  
+
   render() {
     return (
       <div>
           <div className="ball" style={{top:this.state.y+"px",left:this.state.x+"px"}}></div>
           {!this.state.renderBall && <button className="start" onClick={this.buttonClickHandler}>start</button>}
-          <div className="heading-timer">Timer</div>
-          <div className="hole"></div>
+          <div className="heading-timer">{this.timer()}</div>
+         <div className="hole"></div>
       </div>
   );
-  }
+  };
 }
+
 
 export default Timer;
